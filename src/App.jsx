c9b09f6fff47a5aef529fb13a3de0ed26b5973f7
palakjain8ineycom <Palak.Jain8@in.ey.com>
2023-04-12 12:03:09 +0530

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,11 @@ function App() {
     
     setMessages(newMessages);
 
-    // Initial system message to determine ChatGPT functionality
-    // How it responds, how it talks, etc.
     setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
+  async function processMessageToChatGPT(chatMessages) { /
 
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
@@ -49,14 +44,11 @@ function App() {
     });
 
 
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act. 
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
-        systemMessage,  // The system message DEFINES the logic of our chatGPT
-        ...apiMessages // The messages from our chat with ChatGPT
+        systemMessage,  
+        ...apiMessages 
       ]
     }
 
